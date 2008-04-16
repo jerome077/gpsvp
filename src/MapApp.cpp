142785cb9c3571fbfa09bb89585c95f7ff438559
vsevolod.shorin <vsevolod.shorin@1f4ea394-5b4a-0410-8690-43b0940b6c7b>
2008-04-16 04:52:34 +0000

    New copyrite notice in about box

--- src/MapApp.cpp
+++ src/MapApp.cpp
@@ -3839,10 +3839,17 @@ void CMapApp::RegisterFileTypes()
 }
 void CMapApp::About()
 {
-	wchar_t buffer[1000];
-	swprintf(buffer, 1000, L"%s %S\n© Vsevolod Shorin (%s) 2005-2007\n%s %S\n%s.\nhttp://gpsvp.com/",
-		L("gpsVP version"), sVersion, L("Vsevolod Shorin"), L("Built on"), __DATE__, 
-		L("GPS navigation software for PCs, handhelds and smartphones"));
+	wchar_t buffer[10000];
+	swprintf(buffer, 10000, L"%s %S\n%s %S\n%s.\n%s",
+		L("gpsVP version"), sVersion, L("Built on"), __DATE__, 
+		L("GPS navigation software for PCs, handhelds and smartphones"),
+		L"Copyright (c) 2005-2008, Vsevolod E. Shorin\nAll rights reserved.\n\n"
+		L"Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\n\n"
+		L"* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\n"
+		L"* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\n"
+		L"* Neither the name of Vsevolod E. Shopin nor the names of contributors may be used to endorse or promote products derived from this software without specific prior written permission.\n\n"
+		L"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n"
+		);
 	MessageBox(m_hWnd, buffer, L("About"), MB_ICONINFORMATION);
 }
 
