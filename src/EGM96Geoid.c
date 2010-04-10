5b99c359f3f6b01fd7f6cb4125dda823f265ca4a
cybermaus <cybermaus@1f4ea394-5b4a-0410-8690-43b0940b6c7b>
2010-04-10 20:28:56 +0000

    Geoid correction matrix: Updated values Atlantic & east America's. Matrix is now complete

--- src/EGM96Geoid.c
+++ src/EGM96Geoid.c
@@ -37,7 +37,7 @@ Implementing the full n-order function from NASA may provide more precision
 then needed, at too high resource cost (for small handheld devices). This function
 operates from a simple lookup matrix with 10 degree resolution and interpolates the
 current position in the matrix. Only a few few integer and float calculations are used,
-the size is less than 2kB and the altitude correction found seems to be correct
+the size is less than 2kB and the altitude correction found seems to often be correct
 within 1 meter of the full EGM96 or EGM2008 calculation.
 
 This does not imply the altitude value is correct to within 1 meter, as normal GPS
@@ -62,10 +62,12 @@ fixed user provided separation can be used in stead of the position dependant ma
 // The following values are (to be) initialized from the m_rsEGM96Altitude registry entry
 static char	iEGM96Mode;
 	// Correction mode from registry or ini file
-	// 0 = Auto: Perform correction if GPS does not already do this
-	// 1 = Never: Do not perform any correction
-	// 2 = Always: Always peform correction
-	// 3 = User: Use user provided separation value
+	// +127 = 0 = Auto: Perform correction if GPS does not already do this
+	//                  So if NMEA field 11 = zero, calculate
+	// +126 = 1 = Never: Do not perform any correction
+	// +125 = 2 = Always: Always peform correction
+	//                  Add existing field 11 to heigth, then subtract our calculation
+	// -99..99  = User provided separation value
 	// 4 = Sirf: GPS does provides info, but it still needs to be applied
 static double dEGM96Separation;
 	// Latest separation info from GPS or Array (depending on mode)
@@ -74,6 +76,7 @@ static double dEGM96Separation;
 
 // Array with WGS84 to EGM96 altitude separation
 // Warning: array is tilted with north to the right.
+// http://cddis.nasa.gov/926/egm96/geoid_050.gif
 static const char geoid[19*37] = {
 /*       -90,-80,-70,-60,-50,-40,-30,-20,-10,  0, 10, 20, 30, 40, 50, 60, 70, 80, 90 
   -180 */-30,-53,-62,-46,-17, 21, 48, 50, 35, 21, 13,  4, -8,-13, -8,  0,  3,  4, 14,
@@ -82,15 +85,15 @@ static const char geoid[19*37] = {
 /*-150 */-29,-51,-57,-32,-16, -7, -1, -1,  5, 13,  1, -7,-16,-21,  0, 13, -1,  0, 14,
 /*-140 */-29,-48,-52,-30,-17,-12, -9, -9, -3,  1,-11,-23,-28,-31,-12, 11, -3, -2, 14,
 /*-130 */-29,-42,-44,-27,-16,-13,-13,-11, -9,-13,-29,-39,-40,-37,-18,  0, -8, -3, 14,
-/*-120 */-29,  0,  0,  0,  0,  0,  0,  0,  0,-23,  0,  0,  0,  0,  0,  0,  0,  0, 14,
-/*-110 */-29,  0,  0,  0,  0,  0,  0,  0,  0,-20,  0,  0,  0,  0,  0,  0,  0,  0, 14,
-/* 100 */-29,  0,  0,  0,  0,  0,  0,  0,  0,-14,  0,  0,  0,  0,  0,  0,  0,  0, 14,
-/* -90 */-29,  0,  0,  0,  0,  0,  0,  0,  0, -4,  0,  0,  0,  0,  0,  0,  0,  0, 14,
-/* -80 */-29,  0,  0,  0,  0,  0,  0,  0,  0, 14,  0,  0,  0,  0,  0,  0,  0,  0, 14,
-/* -70 */-29,  0,  0,  0,  0,  0,  0,  0,  0, 14,  0,  0,  0,  0,  0,  0,  0,  0, 14,
-/* -60 */-29,  0,  0,  0,  0,  0,  0,  0,  0,-13,  0,  0,  0,  0,  0,  0,  0,  0, 14,
-/* -50 */-29,  0,  0,  0,  0,  0,  0,  0,  0,-26,  0,  0,  0,  0,  0,  0,  0,  0, 14,
-/* -40 */-29,  0,  0,  0,  0,  0,  0,  0,  0,-19,  0,  0,  0,  0,  0,  0,  0,  0, 14,
+/*-120 */-29,-39,-37,-24,-10,-13,-10, -6,-11,-23,-39,-48,-42,-23,-16,-16,-15, -1, 14,
+/*-110 */-29,-37,-33,-22,-11,-10, -8, -2, -8,-20,-29,-34,-30,-18,-17,-31,-25,  4, 14,
+/* 100 */-29,-28,-26,-17, -9, -7, -4, -4,-12,-14,-12, -7,-23,-25,-24,-42,-28,  3, 14,
+/* -90 */-29,-22,-21,-11, -3, -1,  1, -1, -9, -4,  1,-11,-27,-33,-35,-47,-26, 11, 14,
+/* -80 */-29,-25,-10, -4,  5,  7,  8,  8, -1, 14,  1,-19,-32,-34,-40,-42,-18, 15, 14,
+/* -70 */-29,-22,  4,  9, 11, 23, 37, 34, 27, 14,-11,-47,-52,-35,-27,-23,  3,  7, 14,
+/* -60 */-29,-21,  5, 20, 12, 13, 18, 20,  3,-13,-42,-47,-41,-23,-13,  5, 23, 18, 14,
+/* -50 */-29,-20,  0, 20,  2, -3,  3, -8,-19,-26,-43,-33,-18,  1, 24, 29, 30, 28, 14,
+/* -40 */-29,-18,  3, 22,  3, -7, -9, -7,-12,-19,-16,-10, 16, 32, 45, 49, 46, 34, 14,
 /* -30 */-29,-17,  1, 24, 11,  6,  3, -6,-10,  2,  3, 17, 30, 59, 62, 64, 57, 36, 14,
 /* -20 */-29,-13,  2, 21, 20, 21, 10, -1,  4, 13, 17, 25, 34, 52, 62, 61, 60, 32, 14,
 /* -10 */-29,-10,  9, 17, 27, 23, 15, 12, 13, 19, 33, 30, 43, 49, 58, 55, 57, 32, 14,
@@ -131,10 +134,13 @@ void EGM96Geoid(double dLat, double dLon, double *dAlt, double *dSep)
 	switch (iEGM96Mode) {
 
 	case 0: // "Auto" correct Altitudes if GPS does not provide separation
-		if (*dSep != 0) return;
-		// no break, continue with Always case
+		if (*dSep != 0) return; // means GPS already provides separation
+		// no return, continue with case 2: Always
 
 	case 2: // "Always" correct Altitudes, do not check if GPS already does this
+		// First undo chipset correction (if any)
+		*dAlt += *dSep;	
+
 		// Array index of position
 		dLatDelta = modf (dLat/10, &dIdx);
 		iLatIdx = dIdx;
@@ -146,14 +152,20 @@ void EGM96Geoid(double dLat, double dLon, double *dAlt, double *dSep)
 		*dSep =
 			((geoid[iIdx] - geoid[iIdx+1] + geoid[iIdx+20] - geoid[iIdx+19])
 			* (dLonDelta * dLatDelta / 100)) + geoid[iIdx];
+
+		// Apply correction
  		*dAlt -= *dSep;
 		return;
+		
+	case 3: // User determined fixed altitude correction
+		// First undo chipset correction (if any)
+		*dAlt += *dSep;	
 
-	case 3: // Fixed altitude correction
+		// Apply user correction
 		*dAlt -= dEGM96Separation;
 		return;
 
-	case 4: // "Sirf" if GPS does profide separation, it still needs to be added
+	case 4: // "Sirf" if GPS does profide separation, but it still needs to be added
  		*dAlt -= *dSep;
 		return;
 
