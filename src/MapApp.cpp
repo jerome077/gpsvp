ce0a32b1fcab5474483a56eb2707f3ace594e482
jerome077 <jerome077@1f4ea394-5b4a-0410-8690-43b0940b6c7b>
2011-01-23 20:18:24 +0000

    v0.4.22

--- src/MapApp.cpp
+++ src/MapApp.cpp
@@ -64,7 +64,7 @@ extern HINSTANCE g_hInst;
 
 extern int MakeScancode(WPARAM wParam, LPARAM lParam);
 
-const CVersionNumber g_gpsVPVersion(0, 4, 21);
+const CVersionNumber g_gpsVPVersion(0, 4, 22);
 
 class CNmeaCommandsDlg : public CMADialog
 {
