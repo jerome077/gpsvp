102bdf4a600a5ca0134f0dd0c34d727f5d07118c
jerome077@googlemail.com <jerome077@googlemail.com@1f4ea394-5b4a-0410-8690-43b0940b6c7b>
2012-02-05 00:04:31 +0000

    Version number raised to 0.4.25 for the next developments

--- src/MapApp.cpp
+++ src/MapApp.cpp
@@ -64,7 +64,7 @@ extern HINSTANCE g_hInst;
 
 extern int MakeScancode(WPARAM wParam, LPARAM lParam);
 
-const CVersionNumber g_gpsVPVersion(0, 4, 24);
+const CVersionNumber g_gpsVPVersion(0, 4, 25);
 
 class CNmeaCommandsDlg : public CMADialog
 {
